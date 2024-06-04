dc01db3df3ddbf7cfbf667294046c418d08e9a22
Jhonconnors <anonimat.jk@gmail.com>
2024-06-04 18:27:28 -0400

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+function Factorial(){
+
+    let num = document.getElementById("inputNumero").value;
+
+    let factorial = 1;
+
+    if(num < 0){
+        document.getElementById("resultado").innerHTML = "El factorial solo se calcula para numeros positivos.";
+        return;
+    }else if(isNaN(num)) {
+        document.getElementById("resultado").innerHTML = "Por favor ingrese valores reales";
+        return;
+    }else {
+
+        for(let i = num; i>0; i--) {
+			
+			factorial = factorial*i;
+		}
+
+        document.getElementById("resultado").innerHTML = "El factorial de:  " + num +" es: " + factorial;
+    }
+
+}
+
+
+function Fecha(){
+
+    let fecha1 = document.getElementById("inputFecha").value;
+
+    document.getElementById("resultado").innerHTML = "la cantidad de dias son: " + fecha1;
+}
